30c1c73b78ebaa9948a1b4e618d4cf734e15d4aa
Janario Oliveira <janario.oliveira@gmail.com>
2018-04-17 16:22:10 -0300

    Jackson conf to avoid creation of setters and to serialize/deserialize Instant as long epoch millis

---
+++ src/main/java/me/janario/RestConfiguration.java
@@ -0,0 +1,37 @@
+package me.janario;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+public class RestConfiguration {
+
+	@Bean
+	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+		return new Jackson2ObjectMapperBuilder() {
+
+			@Override
+			public void configure(ObjectMapper objectMapper) {
+				super.configure(objectMapper);
+
+				objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+				objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+				objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.ANY);
+
+				objectMapper.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+				objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+			}
+		};
+	}
+}
